33a668ac2f6ceb4a51feca1b1a1aa4563a856c3e
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-04-08 18:06:12 +0200

    da sistemare l'ordine degli albums

--- js/script.js
+++ js/script.js
@@ -7,12 +7,21 @@ let app = new Vue (
     mounted: function(){
 
       let self = this;
-        
+
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then(function (response) {
 
         self.albums = response.data.response;
+        self.albums.sort(function(a, b){return a-b});
       });
+    },
+    methods: {
+      orderedAlbums: function(){
+            this.albums.sort((a, b) => {
+                return a.year - b.year;
+            });
+
+
     }
   }
 );
