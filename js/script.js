693c7daf2905712f526b84396fd03af38051a82c
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-04-09 12:11:10 +0200

    aggiunto filtro per generi

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ let app = new Vue (
   {
     el: '#app',
     data: {
-      albums: []
+      albums: [],
+      genres: [], //array generi
+      genreSelect: '' //collegamento alla select per generi
     },
     mounted: function(){
 
@@ -13,11 +15,15 @@ let app = new Vue (
 
         self.albums = response.data.response;
         // filtro in ordine gli album dal  piu vecchio
-        self.albums.sort((a, b)=>{return a.year - b.year});
+        self.albums.sort((a, b) => {return a.year - b.year});
+
+        // ciclo gli albums per genere e poi pusho nel nuovo array
+        self.albums.forEach((item, i) => {
+          if(self.genres.includes(item.genre) == false){
+            self.genres.push(item.genre);
+          }
+        });
       });
     },
-    methods: {
-
-    }
   }
 );
